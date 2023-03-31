a4c111cb3781c550a8c5cb5c03dd8ef7b781d415
Imad Asserne <imad.asserne@gmail.com>
2023-03-31 22:31:40 +0000

    fix name of component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import { AppRoutingModule } from '../app-routing.module';
 import { DashboradComponent } from './pages/dashborad/dashborad.component';
 import { NavComponent } from './body/nav/nav.component';
-import { AppRoutingModule } from '../app-routing.module';
-
-
 
 @NgModule({
   declarations: [
+    AdminComponent,
     DashboradComponent,
     NavComponent
   ],
   imports: [
     CommonModule,
     AppRoutingModule
-
   ]
 })
 export class AdminModule { }
